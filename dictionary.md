be47a6e59a528dc3d18b77f6dae9ad628e059877
iblancett <isabel.blancett@students.olin.edu>
2018-10-09 20:54:02 -0700

    Began traffic exercise

---
+++ dictionary.md
@@ -0,0 +1,5 @@
+- TensorFlow = Google's machine learning framework to create and train deep learning models
+
+- tensors = a mathematical representation that can specify magnitude and MULTIPLE directions
+
+- placeholders = values that are unassigned by the programmer, but assigned during runtime
--- ex1.py
+++ ex1.py
@@ -1,8 +1,18 @@
+# Import `tensorflow`
 import tensorflow as tf
 
+# Initialize two constants
 x1 = tf.constant([1,2,3,4])
 x2 = tf.constant([5,6,7,8])
 
+# Multiply
 result = tf.multiply(x1, x2)
 
-print(result)
+# Intialize the Session
+sess = tf.Session()
+
+# Print the result
+print(sess.run(result))
+
+# Close the session
+sess.close()
